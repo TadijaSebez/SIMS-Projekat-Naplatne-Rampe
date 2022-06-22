617b4f53a3b9bc9b6604a31267c564b8c7239fde
Anastasija Draskovic <draskovic.sv67.2020@uns.ac.rs>
2022-06-22 15:03:45 +0200

    Implementing connection to database

---
+++ NaplatnaRampa/NaplatnaRampa/Globals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using MongoDB.Driver;
+
+namespace NaplatnaRampa
+{
+    internal class Globals
+    {
+        public static IContainer container;
+        public static IMongoDatabase database;
+        public static void Load()
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule<ProgramModule>();
+            container = containerBuilder.Build();
+
+            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://anastasija:anastasija2001@cluster0.tlsbsly.mongodb.net/test");
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            var client = new MongoClient(settings);
+            database = client.GetDatabase("SIMS");
+        }
+    }
+}
---
+++ NaplatnaRampa/NaplatnaRampa/ProgramModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace NaplatnaRampa
+{
+    internal class ProgramModule : Module
+    {
+    }
+}
