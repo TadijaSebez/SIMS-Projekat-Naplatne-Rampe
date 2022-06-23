9263ea6f70b6ccc54e10c1269d79a52fdc611c54
Tadija <tadijamusic@gmail.com>
2022-06-23 16:25:29 +0200

    Fix TableUsers

--- NaplatnaRampa/NaplatnaRampa/application/App.cs
+++ NaplatnaRampa/NaplatnaRampa/application/App.cs
@@ -16,13 +16,9 @@ namespace NaplatnaRampa.application
         public static void Main(string[] args)
         {
             Globals.Load();
-            
 
-         //   UserRepository repo = new UserRepository();
-           // repo.Insert(new User("Ime", "Prezime", "mejl", "pass", "tel", new Address("ulica", 4, new Place(132, "nesto")._id)._id));
-            
-           // Login login = new Login();
             Application.Run(new Login(Globals.database));
+            //Application.Run(new TableUsers());
         }
     }
 }
